8cddf86a6cb2d3ace749e87241dfa3594a68ff7b
Arjan de Ruijter <rra.am1a.2016.blok4@gmail.com>
2017-06-02 09:06:14 +0200

    Controller

--- js/testController.js
+++ js/testController.js
@@ -1,10 +1,10 @@
 app.controller("testController", function($scope) {
-      $scope.voornaam = "Arjanus";
-      $scope.tussenvoegsel = "de";
-      $scope.achternaam = "Ruijter";
+      $scope.firstname = "Arjanus";
+      $scope.infix = "de";
+      $scope.lastname = "Ruijter";
       $scope.color = "rgb(122, 233, 120)";
       $scope.size = "150px";
       $scope.naamVolledig = function () {
-         return $scope.voornaam + " " + $scope.tussenvoegsel + " " + $scope.achternaam;
+         return $scope.firstname + " " + $scope.infix + " " + $scope.lastname;
       };
 });
