87e0cf93187ce117b0668ce9128709e800f23cb3
Arjan de Ruijter <rra.am1a.2016.blok4@gmail.com>
2017-06-08 08:56:13 +0200

    Last commit

--- index.php
+++ index.php
@@ -5,8 +5,12 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <meta http-equiv="X-UA-Compatible" content="ie=edge">
       <title>Document</title>
+
       <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js"></script>
       <link href="https://fonts.googleapis.com/css?family=Arvo|Barrio" rel="stylesheet">
+      
+      <!-- Bootstrap -->
+      <link href="css/bootstrap.min.css" rel="stylesheet">
       <link rel="stylesheet" href="./css/style.css">
    </head>
    <body>
@@ -24,9 +28,42 @@
          <ul>
             <li ng-repeat="x in jsonString">{{x.id}} {{x.firstname}} {{x.infix}}{{x.lastname}} {{x.haircolor}} </li>
          </ul>
-         <hr>
+         <table class="table">
+            <thead>
+                  <tr>
+                        <th>Firstname</th>
+                        <th>Lastname</th>
+                        <th>Email</th>
+                        <th>haircolor</th>
+                  </tr>
+            </thead>
+    <tbody>
+      <tr>
+        <td>John</td>
+        <td>Doe</td>
+        <td>john@example.com</td>
+      </tr>
+      <tr>
+        <td>Mary</td>
+        <td>Moe</td>
+        <td>mary@example.com</td>
+      </tr>
+      <tr>
+        <td>July</td>
+        <td>Dooley</td>
+        <td>july@example.com</td>
+      </tr>
+    </tbody>
+
+         </table>
+         
                
       </div>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
    </body>
 </html>
 
